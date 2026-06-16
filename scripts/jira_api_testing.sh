dc621c67a8f559aa1871c10819ba290966da6a22
bradly <bradlycarpenterza@gmail.com>
2026-06-16 03:43:34 +0200

    refactor: simplify script

--- scripts/jira_api_testing.sh
+++ scripts/jira_api_testing.sh
@@ -1,28 +1,15 @@
 source .env
 
-urlencode() {
-    local raw_url="$1"
-    printf '%s' "$raw_url" | jq -sRr @uri
-}
-
-handle_today() {
-  local encoded_jql=$(urlencode 'project = "LUM" AND assignee = currentuser()')
-
-  local response=$(
-    curl --request GET \
-      --url "https://$TEST_JIRA_DOMAIN/rest/api/3/search/jql?jql=$encoded_jql" \
-      --user "$TEST_JIRA_EMAIL:$TEST_JIRA_API_KEY" \
-      --header "Accept: application/json" \
-      --silent)
-
-  echo "$response" | jq '.'
-}
-
 case $1 in
   work)
     case $2 in
       today)
-        handle_today
+        curl --get \
+          --url "https://$TEST_JIRA_DOMAIN/rest/api/3/search/jql" \
+          --data-urlencode 'jql=project = "LUM" AND assignee = currentuser()' \
+          --user "$TEST_JIRA_EMAIL:$TEST_JIRA_API_KEY" \
+          --header "Accept: application/json" \
+          --silent | jq '.'
         ;;
       *)
         echo "Uknown argument: $2"
