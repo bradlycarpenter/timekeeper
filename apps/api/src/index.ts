dc8aff2d2421ddb39dfab7914d708e2f64e3311f
bradly <bradlycarpenterza@gmail.com>
2026-06-16 17:48:42 +0200

    testing: add began tickets fetch

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,6 @@
-import { serve } from '@hono/node-server'
+import { z } from 'zod'
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
@@ -7,8 +8,64 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/work/today', (res) => {
-  return res.text("Today's work")
+app.get('/work/commit', async (c) => {
+  let fetchErrors: string[] = []
+
+  const beganTickets = await fetch(
+    `https://${process.env.TEST_JIRA_DOMAIN}/rest/api/3/search/jql?` +
+      `jql=${encodeURI('assignee = currentUser() AND status changed TO "In Progress" AFTER startOfDay() AND status = "In Progress"')}` +
+      '&fields=summary',
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Basic ${btoa(`${process.env.TEST_JIRA_EMAIL}:${process.env.TEST_JIRA_API_KEY}`)}`,
+      },
+    },
+  )
+    .then(async (res) => {
+      const json = await res.json()
+      if (!res.ok) {
+        console.log(
+          'Error fetching tickets with began status, response: ',
+          json,
+        )
+        fetchErrors.push('Error fetching tickets with began status.')
+        return undefined
+      }
+      try {
+        return z
+          .object({
+            issues: z
+              .object({
+                expand: z.string(),
+                id: z.string(),
+                self: z.string(),
+                key: z.string(),
+                fields: z.object({
+                  summary: z.string(),
+                }),
+              })
+              .array(),
+            isLast: z.boolean(),
+          })
+          .parse(json)
+      } catch (e) {
+        console.error(
+          `Error parsing tickets with began status query, res: ${JSON.stringify(json)}, e: ${e}`,
+        )
+        fetchErrors.push('Error parsing tickets with began status query.')
+        return undefined
+      }
+    })
+    .catch((e) => {
+      console.error(e)
+      fetchErrors.push('Error fetching tickets with began status')
+      return undefined
+    })
+
+  if (fetchErrors.length > 0) return c.json({ error: 'Error' }, 500)
+
+  return c.json({ tickets: beganTickets })
 })
 
 serve(
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -4,17 +4,27 @@ import { createFileRoute } from '@tanstack/react-router'
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
-  const { data, isPending } = useQuery({
+  const { data, isPending, isError } = useQuery({
     queryKey: ['data'],
     queryFn: async () =>
-      await fetch('/api/').then(async (result) => await result.text()),
+      await fetch('/api/work/commit').then(async (res) => {
+        if (!res.ok) throw new Error('Error fetching work')
+        return await res.json()
+      }),
     staleTime: 1_000 * 5,
+    retry: 1,
   })
 
   return (
     <div className="flex flex-1 h-screen flex-col items-center justify-center">
       <h2>Fetch Result</h2>
-      {isPending ? <p>Fetching results..</p> : <p>{data}</p>}
+      {isPending ? (
+        <p>Fetching results..</p>
+      ) : isError ? (
+        <p>Error fetching results</p>
+      ) : (
+        <p>{JSON.stringify(data)}</p>
+      )}
     </div>
   )
 }
