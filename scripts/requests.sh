87a357b7d7fd31ca0de8f4e3f3f4c3ddca141c6d
bradly <bradlycarpenterza@gmail.com>
2026-06-16 16:24:32 +0200

    feat: all needed jira commands

--- scripts/requests.sh
+++ scripts/requests.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+get_issues() {
+  local jql=$1
+  curl --get \
+    --url "https://$TEST_JIRA_DOMAIN/rest/api/3/search/jql" \
+    --data-urlencode "jql=$jql" \
+    --data-urlencode 'fields=summary' \
+    --user "$TEST_JIRA_EMAIL:$TEST_JIRA_API_KEY" \
+    --header "Accept: application/json" \
+    --silent | jq '.'
+}
+
 case $1 in
   work)
     case $2 in
-      today)
-        curl --get \
-          --url "https://$TEST_JIRA_DOMAIN/rest/api/3/search/jql" \
-          --data-urlencode 'jql=project = "LUM" AND assignee = currentuser()' \
-          --user "$TEST_JIRA_EMAIL:$TEST_JIRA_API_KEY" \
-          --header "Accept: application/json" \
-          --silent | jq '.'
+      progress)
+        get_issues 'assignee = currentUser() AND status = "In Progress" AND sprint in openSprints()'
+        ;;
+      began)
+        get_issues 'assignee = currentUser() AND status changed TO "In Progress" AFTER startOfDay() AND status = "In Progress"'
+        ;;
+      pr)
+        get_issues 'assignee = currentUser() AND status changed TO "pr" AFTER startOfDay() AND status = "pr"'
+        ;;
+      done)
+        get_issues 'assignee = currentUser() AND status changed TO "Done" AFTER startOfDay() AND status = "Done"'
         ;;
       *)
         echo "Uknown argument: $2"
