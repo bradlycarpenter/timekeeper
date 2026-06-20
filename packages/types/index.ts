f7dada856e6167a62b119128359763372b95a5b1
bradly <bradlycarpenterza@gmail.com>
2026-06-20 21:27:11 +0200

    feat: add types package

---
+++ packages/types/index.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const warpProjectSchema = z.object({
+  TaskId: z.number(),
+  Name: z.string(),
+  IsActive: z.boolean(),
+  Created_On: z.string(),
+  Updated_On: z.string(),
+  Client: z.object({
+    GroupId: z.number(),
+    Name: z.string(),
+    Currency: z.string(),
+  }),
+})
