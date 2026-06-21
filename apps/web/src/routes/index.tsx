7993b5834a0505e920d545fed78a7081fc08f308
bradly <bradlycarpenterza@gmail.com>
2026-06-21 12:38:14 +0200

    feat: project search

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,17 +1,20 @@
-import z from 'zod'
-import { Check } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import type { WarpProject } from '@tk/types'
+import { ScrollArea } from '#/components/ui/scroll-area'
 import { createFileRoute } from '@tanstack/react-router'
-import { Spinner } from '#/components/ui/spinner'
+import type { WarpProject } from '@tk/types'
+import { warpProjectSchema } from '@tk/types'
+import { useEffect, useState } from 'react'
+import z from 'zod'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   const [token, setToken] = useState('')
   const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
   const [projects, setProjects] = useState<WarpProject[]>([])
   const [storageIsReady, setStorageIsReady] = useState(false)
+  const [page, setPage] = useState(1)
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     if (token) localStorage.setItem('token', JSON.stringify({ token }))
@@ -26,6 +29,36 @@ function Home() {
     setStorageIsReady(true)
   }, [setToken])
 
+  useEffect(() => {
+    if (!token) return
+    ;(async () => {
+      setLoading(true)
+      try {
+        const newProjects = await fetch(`/api/sheets/projects/${page}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then(async (res) => {
+          if (!res.ok) {
+            throw new Error('Error fetching projects.')
+          }
+          const json = await res.json()
+          const parseResult = warpProjectSchema.array().safeParse(json)
+          if (!parseResult.success) {
+            throw new Error('Error parsing projects')
+          }
+          return parseResult.data
+        })
+        setProjects(newProjects)
+      } catch (e) {
+        console.log(e)
+        setError('Error fetching projects')
+      }
+      setLoading(false)
+    })()
+  }, [token, setProjects, setError, page, setLoading])
+
   if (!storageIsReady) {
     return (
       <div className="flex flex-1 h-screen flex-col items-center justify-center space-y-2">
@@ -38,6 +71,7 @@ function Home() {
 
   return (
     <div className="flex flex-1 h-screen flex-col items-center justify-center space-y-2">
+      {loading && <p>Loading...</p>}
       {token !== '' ? (
         <div className="flex gap-2 px-4 p-1 justify-center items-center ">
           <p>Token Set</p>
@@ -50,7 +84,7 @@ function Home() {
             const email = formData.get('email')
             const password = formData.get('password')
             try {
-              const newToken = await fetch('api/sheets/auth', {
+              const newToken = await fetch('/api/sheets/auth', {
                 method: 'POST',
                 headers: {
                   'Content-Type': 'application/json',
@@ -97,6 +131,70 @@ function Home() {
           </button>
         </form>
       )}
+      <p>Page: {page}</p>
+      <label>Table Filter on Client Name</label>
+      <input
+        className="border p-1 rounded-2xl"
+        onChange={(e) => {
+          e.preventDefault()
+          setFilter(e.target.value)
+        }}
+      />
+      {projects.length > 0 && (
+        <ScrollArea className="h-72 w-2xl">
+          <table className="w-full text-left">
+            <thead>
+              <tr>
+                <th>Task ID</th>
+                <th>Name</th>
+                <th>Client Group ID</th>
+                <th>Client Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {projects
+                .filter((project) => {
+                  if (filter === '') {
+                    return project
+                  } else if (
+                    project.Client.Name.toLowerCase().includes(
+                      filter.toLowerCase(),
+                    )
+                  ) {
+                    return project
+                  }
+                })
+                .map((project) => (
+                  <tr key={project.TaskId}>
+                    <td>{project.TaskId}</td>
+                    <td>{project.Name}</td>
+                    <td>{project.Client.GroupId}</td>
+                    <td>{project.Client.Name}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </ScrollArea>
+      )}
+      <div className="flex gap-2">
+        <button
+          className="border rounded-2xl px-2 p-1"
+          onClick={() => {
+            if (page < 1) return
+            setPage(page - 1)
+          }}
+        >
+          Previous Page
+        </button>
+        <button
+          className="border rounded-2xl px-2 p-1"
+          onClick={() => {
+            setPage(page + 1)
+          }}
+        >
+          Next Page
+        </button>
+      </div>
       {error && <p className="text-red-500 font-semibold">{error}</p>}
     </div>
   )
