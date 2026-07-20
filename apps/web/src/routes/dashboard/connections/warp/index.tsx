a5276db5d5024fc14f3598748a41ef6031fc46fa
bradly <bradlycarpenterza@gmail.com>
2026-07-20 22:03:29 +0200

    feat: warp auth login

--- apps/web/src/routes/dashboard/connections/warp/index.tsx
+++ apps/web/src/routes/dashboard/connections/warp/index.tsx
@@ -9,26 +9,61 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { useForm } from '@tanstack/react-form'
-import { createFileRoute, Link } from '@tanstack/react-router'
-import { Field, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { createFileRoute, Link, useNavigate } from '@tanstack/react-router'
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
 import z from 'zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { responseParse } from '@tk/utils'
+import { Spinner } from '#/components/ui/spinner'
 
 export const Route = createFileRoute('/dashboard/connections/warp/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+  const { isPending, isError, mutate } = useMutation({
+    mutationFn: async (value: { email: string; password: string }) =>
+      fetch('/api/sheets/auth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(value),
+      }).then(async (res) =>
+        responseParse({
+          res,
+          schema: z.object({
+            success: z.boolean(),
+          }),
+          name: 'Warp Auth Result',
+        }),
+      ),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['sheetAuth'] })
+      navigate({ to: '..' })
+    },
+  })
+
   const form = useForm({
     validators: {
       onSubmit: z.object({
-        email: z.email({ message: 'You must provide an email address' }),
-        password: z
-          .string()
-          .min(1, { message: 'You must indicate a password' }),
+        email: z.email({ message: 'You must provide a valid email address' }),
+        password: z.string().min(1, { message: 'You must provide a password' }),
       }),
     },
-    onSubmit: async ({ value }) => {
-      console.log(value)
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: ({ value }) => {
+      mutate(value)
     },
   })
 
@@ -62,21 +97,74 @@ function RouteComponent() {
               }}
             >
               <FieldGroup>
+                <form.Field
+                  name="email"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                        <Input
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          aria-invalid={isInvalid}
+                          placeholder="name@warpdevelopment.com"
+                          autoComplete="off"
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    )
+                  }}
+                />
+                <form.Field
+                  name="password"
+                  children={(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                        <Input
+                          id={field.name}
+                          name={field.name}
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          aria-invalid={isInvalid}
+                          placeholder="Your password"
+                          type="password"
+                          autoComplete="off"
+                        />
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    )
+                  }}
+                />
                 <Field>
-                  <FieldLabel htmlFor="email">Email</FieldLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="name@warpdevelopment.com"
-                    required
-                  />
-                </Field>
-                <Field>
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <Input id="password" type="password" required />
-                </Field>
-                <Field>
-                  <Button type="submit">Login</Button>
+                  <Button
+                    type="submit"
+                    variant={isPending ? 'secondary' : 'default'}
+                    disabled={isPending || isError}
+                    className={cn(isError && 'bg-red-500')}
+                  >
+                    {isPending ? (
+                      <>
+                        <Spinner /> Loading
+                      </>
+                    ) : isError ? (
+                      'We had an issue'
+                    ) : (
+                      'Log in'
+                    )}
+                  </Button>
                 </Field>
               </FieldGroup>
             </form>
